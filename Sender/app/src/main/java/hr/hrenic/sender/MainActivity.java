4a6db9005b25427d8515012061a1902a3a4b0c86
Filip Hrenić <hrenic.filip@gmail.com>
2017-02-15 12:29:42 +0100

    Add basic send logic

--- Sender/app/src/main/java/hr/hrenic/sender/MainActivity.java
+++ Sender/app/src/main/java/hr/hrenic/sender/MainActivity.java
@@ -1,7 +1,14 @@
 package hr.hrenic.sender;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+
+import hr.hrenic.sender.model.Entry;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +16,44 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        findViewById(R.id.send_button).setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        MainActivity.this.send(v);
+                    }
+                }
+        );
+    }
+
+
+    public void send(View view) {
+        ArrayList<Entry> entries = Entry.getEntries();
+        Intent intent = new Intent();
+        intent.setAction(Intent.ACTION_SEND);
+        intent.setType("*/*");
+        intent.putParcelableArrayListExtra("hrenic", entries);
+//        usingParcelable(intent, entries);
+//        usingJSON(intent, entries);
+        startIntent(intent);
+    }
+
+
+    private void usingParcelable(Intent intent, ArrayList<Entry> entries) {
+        intent.putParcelableArrayListExtra("hrenic", entries);
+    }
+
+    private void usingJSON(Intent intent, ArrayList<Entry> entries) {
+        // TODO
+    }
+
+    private void startIntent(Intent intent) {
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
+            Toast.makeText(this, "Intent sent", Toast.LENGTH_SHORT).show();
+        } else {
+            Toast.makeText(this, "No application can handle intent", Toast.LENGTH_LONG).show();
+        }
     }
 }
